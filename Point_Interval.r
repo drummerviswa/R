884996e1855054bbb4421eef4d187884c6a5a50d
Viswanathan P <drummerviswa@gmail.com>
2024-04-28 18:07:05 +0530

    First

---
+++ Point_Interval.r
@@ -0,0 +1,15 @@
+#Point Estimation
+d<-c("P","P","A","P")
+n<-length(d)
+k<-sum(data=="P")
+p<-k/n
+print(p)
+
+#Interval Estimation
+data=c(170,165,160,170,185,187,190)
+total<-length(data)
+f<-mean(data)
+s<-sd(data)
+margin<-qt(0.975,df=total-1)*sqrt(total)
+low<-f-margin
+print(low)
